4f1ea7d14deee1a51da030571d85c0d6271c4ce2
kimmyuung <rlaaudgh1003@daum.net>
2022-08-28 15:28:21 +0900

    WAS 계산기 구현

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.imageio.IIOException;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
-public class CustomWebApplicationServer {
+public class CustomWebApplicationServer { // 서버 구현
     private final int port;
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
@@ -26,6 +27,15 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");
+                // 1. 사용자 요청을 메인 Thread가 처리하도록 함
+                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+                    String line;
+                    while((line = br.readLine()) != "") {
+                        System.out.println(line);
+                    }
+                }catch(Exception e){e.printStackTrace();}
             }
         }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         new CustomWebApplicationServer(8080).start();
+        System.out.println("Aa");
     }
 }
